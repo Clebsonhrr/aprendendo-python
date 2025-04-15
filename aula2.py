89f9885a39380bbb3a7ca097a35cad5998a0a90e
clebson <clebsonhenriqueros@gmail.com>
2025-04-14 21:57:53 -0300

    Aulas de python

---
+++ aula2.py
@@ -0,0 +1,10 @@
+'''
+Aspas triplas se chama DocString podendo escrever comentarios 
+em varia linhas (precisa colocar 3 aspas no inicio e 3 no final)
+'''
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
