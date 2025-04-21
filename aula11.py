7cc4d1229c550d7036146de1de7ed31c9126641b
clebson <clebsonhenriqueros@gmail.com>
2025-04-21 18:12:43 -0300

    aulas11_12>py

---
+++ aula11.py
@@ -0,0 +1,10 @@
+concatenacao = 'Clebson' ' '+ str (55.36) + ' ' 'Henrique'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_clebson = 3 * 'Clebson'
+print(a_dez_vezes)
+print(tres_vezes_clebson)
+
+print(a_dez_vezes, ' ', tres_vezes_clebson, ' ', concatenacao)
+
