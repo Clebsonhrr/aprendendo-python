67ee968ee5f2ceb8c37d7c014916643729013938
clebson <clebsonhenriqueros@gmail.com>
2025-04-21 17:21:11 -0300

    aula8.py

--- aula8.py
+++ aula8.py
@@ -4,4 +4,10 @@
 # O sinal de = é o operador de atribuição. Ele é usado para
 # atribuir um valor a um nome (variável).
 # Uso: nome_variavel = expressão
-
+#nome_completo ='clebson henrique rosa romao'
+#soma_dois_mais_dois = 2 + 2
+#print(nome_completo, soma_dois_mais_dois) 
+nome = 'clebson'
+idade = 30
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'idade:', idade, 'É maior?', maior_de_idade)
