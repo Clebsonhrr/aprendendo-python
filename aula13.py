7e01d000567370f4c571b7d62cda20ae6e3196e2
clebson <clebsonhenriqueros@gmail.com>
2025-04-21 18:48:55 -0300

    aulas13_14.py

---
+++ aula13.py
@@ -0,0 +1,8 @@
+nome = 'Clebson'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+print(imc)
+
+print(nome, 'tem', altura, 'de altura,', 'pesa', peso, 'quilos e o seu IMC é', imc,)
---
+++ aula14.py
@@ -0,0 +1,13 @@
+nome = 'Clebson'
+altura = 1.79
+peso = 98
+imc = peso / altura ** 2
+
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura, pesa {peso} quilos e o seu imc é {imc:.2f}'
+
+print(linha_1)
+
+print(nome, 'tem', altura, 'de altura,', 'pesa', peso, 'quilos e o seu IMC é', imc,)
+        
